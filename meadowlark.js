3778174055a3edec94331caea0bc5d39964efeae
kathulhur <josephkarl19@outlook.com>
2021-08-24 19:41:17 +0800

    Added tests using jest

--- meadowlark.js
+++ meadowlark.js
@@ -16,27 +16,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.render('home'));
+const handlers = require('./lib/handlers');
 
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
+app.get('/', handlers.home);
 
-});
+app.get('/about', handlers.about);
 
 // custom 404 page
-app.use( (req, res) => {
-    res.status(404);
-    res.render('404');
-});
+app.use(handlers.notFound);
 
 
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message);
-    res.status(500);
-    res.render('500');
-});
+app.use(handlers.serverError);
 
 
 
